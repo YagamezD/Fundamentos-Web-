b24835539faa131bef00b8f3bcc8d2e04beb1754
YagamezD <yanisagamez@gmail.com>
2022-06-15 14:31:13 -0500

    Me gusta

---
+++ Javascript/megusta/js/script.js
@@ -0,0 +1,21 @@
+var n1 = 9;
+
+function incrementar() {
+    n1++;
+    var elemento = document.querySelector(".p");
+    elemento.innerText = n1 + " likes(s)";
+}
+var n2 = 12;
+
+function incrementar2() {
+    n2++;
+    var elemento = document.querySelector(".p2");
+    elemento.innerText = n2 + " likes(s)";
+}
+var n3 = 9;
+
+function incrementar3() {
+    n3++;
+    var elemento = document.querySelector(".p3");
+    elemento.innerText = n3 + " likes(s)";
+}
