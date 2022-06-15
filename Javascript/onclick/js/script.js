09d8073d8f9f00cc0adc691f3521d08912052b53
YagamezD <yanisagamez@gmail.com>
2022-06-14 23:50:51 -0500

    Javascript

---
+++ Javascript/onclick/js/script.js
@@ -0,0 +1,15 @@
+function cambiar_login(boton) {
+    boton.innerText = "Logout"
+
+}
+
+function remover(elemento) {
+    elemento.remove();
+
+
+
+}
+
+function alerta() {
+    alert("Ninja was liked");
+}
